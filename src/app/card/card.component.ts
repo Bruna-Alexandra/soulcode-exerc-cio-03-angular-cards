0cd9292d30a80c42104ca156262e5029d20e5b70
Bruna-Alexandra <mittyc4@gmail.com>
2022-03-08 23:43:28 -0300

    commit inicial

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+  @Input()
+  title: string = "aweawe";
+  @Input()
+  price: string = "$0.00";
+  @Input()
+  content1: string = "";
+  @Input()
+  content2: string = "";
+  @Input()
+  content3: string = "";
+  @Input()
+  bg: string = "white";
+  @Input()
+  size: string = '254';
+  @Input()
+  color: string = '#4a4c60';
+  @Input()
+  border: string = '1px grey solid';
+  @Input()
+  index: string = '0';
+  @Input()
+  butcolor: string = "#8d91ea";
+
+} 
